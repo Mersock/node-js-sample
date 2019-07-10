7c4c44746e37b1fea878620e87d39d9785e91808
Phumthawan <knzphumthawan@gmail.com>
2019-07-11 03:11:37 +0700

    add command

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
+const yargs = require('yargs');
 
-const msg = getNotes();
 
-const greenMsg = chalk.green.bold.bgRed(msg);
+//version
+yargs.version('1.1.0');
 
-console.log(greenMsg);
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Add a new note!')
+    }
+});
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler:function () {
+        console.log('Removeing the note!')
+    }
+});
+
+//create list command
+yargs.command({
+   command:'list',
+   describe:'List a note',
+   handler:function () {
+       console.log('List the note!')
+   }
+});
+
+//Create read command
+yargs.command({
+    command:'read',
+    describe:'Read a note',
+    handler:function () {
+        console.log('Read a note')
+    }
+})
+
+//add ,remove,read,list
+
+console.log(yargs.argv);
