54c62a8eae82154cf5fe4a0f8c0afa68aab5015e
Phumthawan <knzphumthawan@gmail.com>
2019-07-16 17:12:00 +0700

    add command add bangkok

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,29 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Bangkok', (error, data) => {
-    console.log(`Error`, error);
-    console.log(`Data`, data);
-});
+//use command as   "node app [address]"
+const address = process.argv[2];
 
-forecast(13.75,100.51667,(error,data) => {
-    console.log(`Error`,error);
-    console.log(`Data`,data);
-});
+if (!address) {
+    console.log('Please provide an address');
+} else {
+
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(`Error`, error);
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(`Error`, error);
+            }
+
+            console.log(data.location);
+            console.log(forecastData)
+        });
+
+    });
+}
+
 
 
