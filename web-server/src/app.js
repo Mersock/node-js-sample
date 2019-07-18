35740594bc1d9c270cc610889333ee415392d251
Phumthawan <knzphumthawan@gmail.com>
2019-07-19 03:46:47 +0700

    add template static pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
 // console.log(__dirname);
 // console.log(__filename);
 // console.log(path.join(__dirname,'../public'));
 
-//set static file
-const publicDirectoryPath = path.join(__dirname,'../public');
+const app = express();
 
+//set static file
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'knz Phumthawan'
+    });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'Weather',
+        name: 'knz Phumthawan'
+    })
+});
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        title: 'Weather',
+        name: 'knz Phumthawan'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send({
