e9dfd1d513f5f3b7051d1f46675de29aee029179
Phumthawan <knzphumthawan@gmail.com>
2019-07-21 18:45:46 +0700

    add query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -44,9 +44,29 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address){
+        return res.send({
+            'error':'You must provide your address'
+        })
+    }
+
     res.send({
-        'weather': 'name',
-        'cast': 'now'
+        forecast: 'It is summer',
+        location: 'Bangkok',
+        address: req.query.address
+    });
+});
+
+app.get('/product', (req, res) => {
+    if (!req.query.search) {
+         return res.send({
+            'error': 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query);
+    res.send({
+        'products': []
     });
 });
 
