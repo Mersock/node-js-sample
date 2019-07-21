de17b4b2d3ddd683f5958382397c4b945186e05f
Phumthawan <knzphumthawan@gmail.com>
2019-07-21 19:03:45 +0700

    add forcast

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 // console.log(__dirname);
 // console.log(__filename);
@@ -44,22 +46,39 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address){
+    if (!req.query.address) {
         return res.send({
-            'error':'You must provide your address'
+            'error': 'You must provide your address'
         })
     }
 
-    res.send({
-        forecast: 'It is summer',
-        location: 'Bangkok',
-        address: req.query.address
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if (error) {
+            return res.send({
+                'error': error
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    'error': error
+                })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address:req.query.address
+            })
+        });
+
     });
 });
 
 app.get('/product', (req, res) => {
     if (!req.query.search) {
-         return res.send({
+        return res.send({
             'error': 'You must provide a search term'
         })
     }
