708afef49ee34e7a5bd08db0687e2060d1ed0700
Phumthawan <knzphumthawan@gmail.com>
2019-07-29 21:21:49 +0700

    fine document

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,14 @@
 // CRUD create read delete
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-mongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database');
     }
@@ -13,7 +16,7 @@ mongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName);
 
     // db.collection('users').insertOne({
-    //     name: 'knz',
+    //     name: 'Meersock',
     //     age: 26
     // }, (error, result) => {
     //     if(error){
@@ -40,17 +43,55 @@ mongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(result.ops);
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description:'read',
-            completed:true
-        }
-    ],(error,result) => {
-        if(error){
-            return console.log('Unable to insert document')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description:'delete',
+    //         completed:false
+    //     }
+    // ],(error,result) => {
+    //     if(error){
+    //         return console.log('Unable to insert document')
+    //     }
 
-        console.log(result.ops);
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('users').findOne({ _id: new ObjectID("5d3ec577590a120b585b7a0f")}, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch');
+    //     }
+
+    //     console.log(result);
+    // });
+
+    // db.collection('users').find({ age: 26}).toArray((error,users) => {
+    //     if(error){
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(users)
+    // });
+
+    // db.collection('users').find({ age: 26}).count((error,count) => {
+    //     if(error){
+    //         return console.log('Unable to fetch');
+    //     }
+    //     console.log(count)
+    // })
+
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5d3ebdc970bab60a0e625d16") }, (error, result) => {
+        if (error) {
+            return console.log('Unable to fetch');
+        }
+        console.log(result);
     })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        if (error) {
+            return console.log('Unable to fetch');
+        }
+        console.log(tasks);
+    })
+
 });
 
