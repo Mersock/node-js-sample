5be5f7e10d8e42e77122c3f2efcdc2744fa57588
Phumthawan <knzphumthawan@gmail.com>
2019-08-03 16:38:12 +0700

    post task

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
-
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 
 mongoose.connect(connectionURL, {
@@ -8,13 +6,3 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true
 });
 
-const Task = mongoose.model('Task', {
-    description:{
-        type: String,
-        required:true,
-    },
-    completed:{
-        type: Boolean,
-        default:false,
-    }
-});
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,8 +12,15 @@ app.use(express.json());
 app.post('/users', (req, res) => {
     const user = new User(req.body);
 
-    user.save().then(() => res.send(user)).catch((error) => res.status(400).send(error));
+    user.save().then(() => res.status(201).send(user)).catch((error) => res.status(400).send(error));
 });
+
+app.post('/tasks', (req,res) => {
+    const task = new Task(req.body);
+    task.save()
+        .then(() => res.status(201).send(task).status(201)).catch((error) => res.status(400).send(error));
+
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
