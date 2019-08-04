72185af2940c4b6c7c4807555287e4261de517cf
Phumthawan <knzphumthawan@gmail.com>
2019-08-04 17:11:02 +0700

    promise chaining

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
-User.findByIdAndUpdate('5d45a43d814e441b935e55ec' , {age : 1}).then(user => {
-    console.log(user);
-    return User.countDocuments({age : 1})
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5d45a43d814e441b935e55ec', { age: 1 }).then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+}
+
+updateAgeAndCount('5d45a43d814e441b935e55ec', 2).then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error);
 })
--- task-manager/playground/promise-chaning-2.js
+++ task-manager/playground/promise-chaning-2.js
@@ -1,11 +1,21 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d45a438814e441b935e55eb').then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed:false })
-}).then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error);
-});
+// Task.findByIdAndDelete('5d45a438814e441b935e55eb').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed:false })
+// }).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed:false });
+    return count
+}
+
+deleteTaskAndCount('5d467d7101899523d705788e').then((count) => {
+    console.log(count);
+}).catch(error => console.log(error));
