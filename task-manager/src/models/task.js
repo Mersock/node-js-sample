6b441332e1691b1a62849acf691eca1f42835b5f
Phumthawan <knzphumthawan@gmail.com>
2019-08-14 04:28:48 +0700

    add timestamp

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
-    description:{
+const taskSchema = new mongoose.Schema({
+    description: {
         type: String,
-        required:true,
+        required: true,
     },
-    completed:{
+    completed: {
         type: Boolean,
-        default:false,
+        default: false,
     },
-    owner:{
+    owner: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref:'User'
+        ref: 'User'
     }
-});
+},{
+    timestamps: true
+})
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
