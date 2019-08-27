1c4e80e3cd2ee024cba630223eaa2bc698ceda92
Phumthawan <knzphumthawan@gmail.com>
2019-08-28 05:49:10 +0700

    set up socket io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set port
 const port = process.env.PORT || 3000;
@@ -8,6 +13,12 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+//set socket io
+io.on('connection', () => {
+    console.log('Connect Web Socket')
+});
+
+//set server
+server.listen(port, () => {
     console.log(`Server in up on port ${port}`);
 });
