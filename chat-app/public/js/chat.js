21235682675ab4108025bebee1d4ea26b1d0b4cc
Phumthawan <knzphumthawan@gmail.com>
2019-08-30 07:02:12 +0700

    add template

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -9,12 +9,36 @@ const socket = io();
 //     socket.emit('increment')
 // })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+//Element
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $message = document.querySelector('#messages')
+
+// Template
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message  
+    })
+    $message.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
     const message = e.target.elements.message.value;
 
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+
         if (error) {
             return console.log(error)
         }
@@ -30,16 +54,13 @@ document.querySelector('#send-location').addEventListener('click', () => {
     }
 
     navigator.geolocation.getCurrentPosition((position) => {
+        $locationButton.setAttribute('disabled','disabled')
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $locationButton.removeAttribute('disabled')
             console.log('Location shared!');
         })
     });
 });
-
-socket.on('message', (message) => {
-    console.log(message)
-})
-
